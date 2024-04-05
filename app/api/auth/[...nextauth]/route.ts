7bf0bc1cf726dc0a157ed256094dd5e5e899af25
sumitnair26 <sumit.nair26@gmail.com>
2024-04-05 18:40:42 +0530

    signin

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -11,8 +11,13 @@ const handler = nextAuth({
                 Password: {label:`password`, type:`password`, placeholder:`Enter Password`}
             },
             async authorize(credentials:any) {
+                console.log(credentials);
+                //backend validation
+                //if validation fails auth library automaticaly shows error on login screen
                 return {
-                    id:"user1"
+                    id:"user1",
+                    name:"Sumit Nair",
+                    email:"sumit.nair26@gmail.com"
                 }
             }
         })
