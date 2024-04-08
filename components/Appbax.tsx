bd419cb730eb0f25bf32c8c5e17288b57d2cde71
sumitnair26 <sumit.nair26@gmail.com>
2024-04-08 15:55:33 +0530

    session

--- components/Appbax.tsx
+++ components/Appbax.tsx
@@ -1,12 +1,13 @@
 "use client"
-import {signIn, signOut } from "@/node_modules/next-auth/react";
+import {signIn, signOut, useSession } from "@/node_modules/next-auth/react";
 export const Appbar = () => {
+    const session = useSession();
     return <div>
         <div className=" flex-container">
                 <button onClick={()=>{signIn();}}>Signin </button>
                 <button onClick={()=>{signOut();}}>Logout </button>
         </div>
-
+        {JSON.stringify(session)}
 
     </div>
 }
