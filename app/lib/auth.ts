3691b85387451cf1b490e6644cde8197f4bdfc61
sumitnair26 <sumit.nair26@gmail.com>
2024-04-09 14:13:28 +0530

    Github Sigle Signon

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,6 +1,7 @@
 
 import CredentialsProvider from '@/node_modules/next-auth/providers/credentials';
 import GoogleProvider from "@/node_modules/next-auth/providers/google";
+import GitHubProvider from "@/node_modules/next-auth/providers/github";
 export const NEXT_AUTH = {
     providers: [
         CredentialsProvider({
@@ -23,6 +24,10 @@ export const NEXT_AUTH = {
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET
+          }),
+        GitHubProvider({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET
           })
     ],
     secret: process.env.NEXTAUTH_SECRET,
